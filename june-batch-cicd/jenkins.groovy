f9d2b8c3462b45f71cddc04ec8f02861683798ce
kapilnagi007 <kapilnagi007@gmail.com>
2026-07-04 19:03:00 +0530

    Initial CICD Commit Changes

--- june-batch-cicd/jenkins.groovy
+++ june-batch-cicd/jenkins.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent any
 
     environment {
-      IMAGE_NAME = "kopilnagi/react-demo-docker"
+      IMAGE_NAME = "kopilnagi/june-batch-cicd"
       IMAGE_TAG = "${BUILD_NUMBER}"
     }
 
@@ -14,56 +14,81 @@ pipeline {
             }
         }
 
+        stage('Debug') {
+            steps{
+                dir('june-batch-cicd') {
+                    sh 'pwd'
+                    sh 'ls -la'
+                    sh 'find . -name package.json'
+                    sh 'node -v'
+                    sh 'npm -v'
+                }
+            }
+        }
+
         stage('Install Dependencies') {
             steps{
-                sh 'npm install'
+                dir('june-batch-cicd') {
+                    sh 'npm install'
+                }
             }
         }
 
         stage('Build Application') {
             steps{
-                sh 'npm run build'
+                dir('june-batch-cicd') {
+                    sh 'npm run build'    
+                }
             }
         }
 
         stage('Test Application') {
             steps{
-                sh 'npm test -- --watchAll==false'
+                dir('june-batch-cicd') {
+                    sh 'npm test -- --watchAll==false'    
+                }
             }
         }
 
         stage('Build Docker Image'){
             steps{
-                sh 'docker build -t $IMAGE_NAME:$IMAGE_TAG .'
+                dir('june-batch-cicd') {
+                    sh 'docker build -t $IMAGE_NAME:$IMAGE_TAG .'
+                }
             }
         }
 
         stage('Push Image') {
           steps {
-            withCredentials([
-              usernamePassword(
-                credentialsId: 'dockerhub-creds',
-                usernameVariable: 'DOCKER_USER',
-                passwordVariable: 'DOCKER_PASS'
-              )
-            ])
-            {
-              sh '''
-              echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
-              docker push $IMAGE_NAME:$IMAGE_TAG
-              '''
+            dir('june-batch-cicd') {
+              withCredentials([
+                  usernamePassword(
+                    credentialsId: 'dockerhub-creds',
+                    usernameVariable: 'DOCKER_USER',
+                    passwordVariable: 'DOCKER_PASS'
+                  )
+                ])
+                {
+                  sh '''
+                  echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+                  docker push $IMAGE_NAME:$IMAGE_TAG
+                  '''
+                }  
             }
+            
           }
         }
 
         stage('Deploy'){
           steps {
-            sh '''
-            docker stop react-app || true
-            docker rm react-app || true
+            dir('june-batch-cicd') {
+                sh '''
+                docker stop react-app || true
+                docker rm react-app || true
 
-            docker run -d --name react-app -p 80:80 $IMAGE_NAME:$IMAGE_TAG
-            '''
+                docker run -d --name react-app -p 80:80 $IMAGE_NAME:$IMAGE_TAG
+                '''
+            }
           }
         }
     }
